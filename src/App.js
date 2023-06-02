7e60b4896d7c52771d27ee24f8a3ae0d74b22419
KrisQ625 <109870555+KrisQ625@users.noreply.github.com>
2023-06-02 19:02:24 +0200

    Finishing touches

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 import Weather from "./Weather";
 function App() {
   return (<div className='App'>
-<div className='container'>
+<div className='container-sm'>
   <Weather defaultCity = "London"/>
   <footer> This project was created by Kristina Vujnovac and is open-sourced on {""}
-    <a href="https://github.com/KrisQ625/weather-app-react" target="_blank" rel=" noopener noreferrer"> GitHub 🖥️</a>
+    <a href="https://github.com/KrisQ625/weather-app-react" target="_blank" rel=" noopener noreferrer"> GitHub 🖥️</a> and hosted on <a href="https://darling-youtiao-4cbd58.netlify.app/"target="_blank" rel=" noopener noreferrer">Netlify</a>.
   </footer>
   </div>
   </div>
